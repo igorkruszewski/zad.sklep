555c817c4d28bbee85420039d558450f25b47dd2
Igor <igorkris719@gmail.com>
2024-03-02 19:41:11 +0100

    Dodaj pliki projektów.

---
+++ zad.sklep/Program.cs
@@ -0,0 +1,21 @@
+﻿const string sklep ="Biedra";
+double bananas =  20.75;
+double jablka = 10.50 ;
+double gruszuńcia = 24.99;
+Console.WriteLine(sklep);
+Console.WriteLine("stan produktów");
+Console.WriteLine();  
+Console.WriteLine("jabka" + ": " + jablka + " złotych za kilogram");
+Console.WriteLine("banany" + ": " + bananas + " złotych za kilogram");
+Console.WriteLine("gruszki" + ": " + gruszuńcia + " złotych za kilogram");
+Console.Write("\n");
+Console.WriteLine("PRZECENY!!!");
+Console.Write("\n");
+bananas = 15.20;
+jablka = 8.99;
+gruszuńcia = 20.00;
+Console.WriteLine("jabka" + ": " + jablka + " złotych za kilogram");
+Console.WriteLine("banany" + ": " + bananas + " złotych za kilogram");
+Console.WriteLine("gruszki" + ": " + gruszuńcia + " złotych za kilogram");
+
+
